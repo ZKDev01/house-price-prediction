4a63312343d8a7810c612ee629d5fda2cb31b4f4
ZKDev01 <rdromaguera2001@gmail.com>
2024-12-11 21:07:11 -0500

    first steps

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+
+
+
+markdown = """
+Proyecto básico de algoritmos de predicción
+"""
+
+def main() -> None:
+  st.title("House Price Prediction")
+  st.markdown(markdown)
+
+if __name__ == '__main__':
+  main()
